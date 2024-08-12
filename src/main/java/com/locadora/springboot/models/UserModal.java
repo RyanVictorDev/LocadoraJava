c91223f10fc632a8c43f9b82fd4af733c11f6619
RyanVictorDev <vryan8294@gmail.com>
2024-08-12 16:17:02 -0300

    feat: creating users table

---
+++ src/main/java/com/locadora/springboot/models/UserModal.java
@@ -0,0 +1,53 @@
+package com.locadora.springboot.models;
+
+import jakarta.persistence.*;
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.util.UUID;
+
+@Entity
+@Table(name = "TB_USERS")
+public class UserModal implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    public enum Role { ADMIN, VISITOR }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID idUser;
+    private String name;
+    private String email;
+    private Role role;
+
+    public UUID getIdUser() {
+        return idUser;
+    }
+
+    public void setIdUser(UUID idUser) {
+        this.idUser = idUser;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+}
